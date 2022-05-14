f899677fbebe58ab51e994d384e38365c80201cc
dankiesgush <simelane13666@gmail.com>
2022-05-14 20:49:25 +0200

    Succesfully Mounted Homepage route

--- index.js
+++ index.js
@@ -15,3 +15,12 @@ app.use(bodyParser.urlencoded({ extended: true }))
 // mount routes
 // By default, the program should come here
 app.use('/', layoutRoutes)
+
+// Going to serve static files
+app.use('/cdn', express.static('public'))
+
+// Set port to work on Azure as well
+const port = process.env.PORT || 3000
+app.listen(port)
+
+console.log('Express server running on port: ', port)
