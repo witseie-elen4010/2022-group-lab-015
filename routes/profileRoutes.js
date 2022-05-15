04f0290005175622df936a11aa759ae9ad7fc29b
Penuel Nkosi <pnkosi270@gmail.com>
2022-05-15 19:45:42 +0200

    Reset password complete

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -9,5 +9,40 @@ const profile = express.Router()
 profile.get('/ForgotPassword', function (req, res) {
   res.sendFile(path.join(__dirname, '../views', 'profile', 'ForgotPassword.html'))
 })
+profile.get('/ResetPassword', function (req, res) {
+  res.sendFile(path.join(__dirname, '../views', 'profile', 'ResetPassword.html'))
+})
 
+profile.post('/api/profile', function (req, res) {
+  let currentUsername = req.body.user
+  let index = -1
+  index = database.RegisteredUsers.findIndex(function (element) {
+    return element.username === currentUsername
+  })
+  if(index === -1) {
+    res.redirect('/profile/ForgotPassword')
+  } else {
+    res.redirect('/profile/ResetPassword')
+  }
+})
+profile.post('/api/profile/reset', function (req, res) {
+  let firstPassword = req.body.password1
+  let secondPassword = req.body.password2
+  let currentUsername = req.body.user
+  let index = -1
+  index = database.RegisteredUsers.findIndex(function (element) {
+    return element.username === currentUsername
+  })
+  console.log(database.RegisteredUsers[index])
+  if(index === -1 || firstPassword !== secondPassword) {
+    res.redirect('/profile/ResetPassword')
+  } else {
+    database.RegisteredUsers[index].password = firstPassword
+    console.log(database.RegisteredUsers[index])
+    res.redirect("/")
+  }
+})
+profile.post('/api/profile/return', function (req, res) {
+  res.redirect("/")
+})
 module.exports = profile
