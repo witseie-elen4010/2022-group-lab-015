dd4c64ef032883a694086c321f9a3a8b564a3656
Samkelo <samujola@gmail.com>
2022-05-15 19:09:30 +0200

    Made the Create Account route and page

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -6,8 +6,35 @@ const express = require('express')
 const req = require('express/lib/request')
 const profile = express.Router()
 
+profile.get('/createAccount', function(req, res){
+  res.sendFile(path.join(__dirname, '../views', 'profile', 'createAccount.html'))
+} )
+
 profile.get('/ForgotPassword', function (req, res) {
   res.sendFile(path.join(__dirname, '../views', 'profile', 'ForgotPassword.html'))
+})
+
+profile.post('/api/profile/createAccount', function (req, res) {
+  let validAccount = false;
+  const usernameval = req.body.username.toLowerCase(), 
+  emailval = req.body.email.toLowerCase(),
+  passwordval = req.body.password.toLowerCase()
+
+  const newUser = [
+    username = usernameval,
+    email = emailval,
+    password = passwordval
+  ]
+
+  //check if all necessary info has been added
+  if (newUser.name !== '' && newUser.email !== '' && newUser.password !== ''){
+    validAccount = true
+    if (validAccount) {
+      database.RegisteredUsers.push(newUser)
+      res.redirect('/')
+    }
+  } else res.redirect('/profile/createAccount')
+
 })
 
 module.exports = profile
