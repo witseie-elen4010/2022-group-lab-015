37cdd9bb077a6f2c212a6adeab1a6394d639d44f
Danielbyte <danielmasango7@gmail.com>
2022-05-22 14:36:28 +0200

    Capture every button pressed on keyboard

--- public/scripts/gamePlay/gamePlay.js
+++ public/scripts/gamePlay/gamePlay.js
@@ -1,1 +1,9 @@
 'use strict'
+const buttonElement = document.querySelectorAll('button')
+buttonElement.forEach((element) => {
+  element.addEventListener('click', keyIsPressed(element.attributes['data-key'].value))
+})
+
+function keyIsPressed (valueOfKeyPressed) {
+  console.log(valueOfKeyPressed)
+}
