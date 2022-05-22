40d174940f1aaa6b115ec55a11f27e0b2bf0189a
Danielbyte <danielmasango7@gmail.com>
2022-05-22 14:54:55 +0200

    capture user input

--- public/scripts/gamePlay/gamePlay.js
+++ public/scripts/gamePlay/gamePlay.js
@@ -1,9 +1,19 @@
 'use strict'
 const buttonElement = document.querySelectorAll('button')
+const wordElement = document.querySelectorAll('.word-row')
+const row = 1
+const letter = 1
+
 buttonElement.forEach((element) => {
-  element.addEventListener('click', keyIsPressed(element.attributes['data-key'].value))
+  element.addEventListener('click', function () {
+    keyIsPressed(element.attributes['data-key'].value)
+  })
 })
 
 function keyIsPressed (valueOfKeyPressed) {
-  console.log(valueOfKeyPressed)
+  FillGameBoard(valueOfKeyPressed)
+}
+
+function FillGameBoard () {
+  console.log(wordElement)
 }
