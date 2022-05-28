086bdc64a4db468188069ab92003782281dc3b06
Penuel Nkosi <pnkosi270@gmail.com>
2022-05-28 19:20:54 +0200

    Database operation, Update password

--- database/databaseOperations.js
+++ database/databaseOperations.js
@@ -14,4 +14,20 @@ operations.ViewUsers = () => {
          dataBase.sql.close()
       })
 }
+
+operations.UpdatePassword = (username,password) => {
+    dataBase.sql.connect(dataBase.configurations).then(pool =>{
+        console.log('Connected to DB')
+         return pool.request()
+         .input('user',dataBase.sql.NVarChar,username)
+         .input('password',dataBase.sql.NVarChar,password)
+         .query('UPDATE WordleUsers SET Password =@password WHERE Username =@user')
+      }).then(result => {
+         console.log(result)
+         dataBase.sql.close()
+      }).catch(error => {
+         console.log(error.message)     
+         dataBase.sql.close()
+      })
+}
 //module.exports = operations
