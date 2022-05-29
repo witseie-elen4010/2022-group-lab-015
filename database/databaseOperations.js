82eb942571c92a7e34ad880ac4ba865e6fa70a40
dankiesgush <simelane13666@gmail.com>
2022-05-29 17:01:28 +0200

    DatabaseOperation-CreateUser

--- database/databaseOperations.js
+++ database/databaseOperations.js
@@ -30,4 +30,23 @@ operations.UpdatePassword = (username,password) => {
          dataBase.sql.close()
       })
 }
-//module.exports = operations
+
+operations.CreateUser = (name,surname,email,username,password) => {
+   dataBase.sql.connect(dataBase.configurations).then(pool =>{
+       console.log('Connected to DB')
+        return pool.request()
+        .input('name',dataBase.sql.NVarChar,name)
+        .input('surname',dataBase.sql.NVarChar,surname)
+        .input('email',dataBase.sql.NVarChar,email)
+        .input('username',dataBase.sql.NVarChar,username)
+        .input('password',dataBase.sql.NVarChar,password)
+        .query('INSERT INTO WordleUsers VALUES(@name,@surname,@email,@username,@password)')
+     }).then(result => {
+        console.log(result)
+        dataBase.sql.close()
+     }).catch(error => {
+        console.log(error.message)     
+        dataBase.sql.close()
+     })
+}
+module.exports = operations
