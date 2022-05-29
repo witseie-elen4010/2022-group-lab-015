5fc1192d3ed9c9128992ccdd0b6ae877df6452d6
dankiesgush <simelane13666@gmail.com>
2022-05-29 17:49:44 +0200

    IntergrationOfLoginToSecureDatabase

--- routes/AccountRoutes.js
+++ routes/AccountRoutes.js
@@ -1,5 +1,6 @@
 'use strict'
 const registeredUsers = require('../database/TemporaltDatabase')
+const dataBase = require('../database/databaseConfig')
 const path = require('path')
 const express = require('express')
 const req = require('express/lib/request')
@@ -9,15 +10,26 @@ Account.get('/homepage', function (req, res) {
     res.sendFile(path.join(__dirname, '../views', 'layouts', 'homepage.html'))
   })
 
-Account.post('/api/Accounts', function (req, res) {
-    const Username = req.body.username
-    const Password = req.body.password
-
-   registeredUsers.RegisteredUsers.forEach((element, index)=>{
-      if(Username === element.username && Password === element.password){
-       res.redirect('../homepage')}
-     })
-    
+  Account.post('/api/Accounts', function (req, res) {
+    const username = req.body.username
+    const password = req.body.password
+    dataBase.sql.connect(dataBase.configurations).then(pool =>{
+      console.log('Connected to DB')
+       return pool.request()
+       .input('username',dataBase.sql.NVarChar,username)
+       .input('password',dataBase.sql.NVarChar,password)
+       .query('SELECT * FROM WordleUsers WHERE Username = @username AND Password = @password')
+    }).then(result => {
+      setTimeout(()=>{
+        if( result.recordset[0] == null){
+          res.redirect('/')
+     }else res.redirect('../homepage')
+      },1000)
+      dataBase.sql.close()
+    }).catch(error => {
+       console.log(error.message)     
+       dataBase.sql.close()
+    })
   })
- 
+
 module.exports = Account
