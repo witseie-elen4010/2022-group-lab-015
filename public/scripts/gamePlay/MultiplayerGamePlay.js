aef53f6ffbc4b6b95f82d4b7c775b8a6ca526155
Danielbyte <danielmasango7@gmail.com>
2022-06-03 07:43:37 +0200

    added Node websocket depebdencies

---
+++ public/scripts/gamePlay/MultiplayerGamePlay.js
@@ -0,0 +1,6 @@
+'use strict'
+// This is the server
+
+// grab every button and row elements
+// const buttonElement = document.querySelectorAll('button')
+// const wordElement = document.querySelectorAll('game-row')
