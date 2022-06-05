459657a9759bf6f1347ad68adc4661dde12228a9
Samkelo <samujola@gmail.com>
2022-06-05 16:33:31 +0200

    headless websocket server

--- public/scripts/gamePlay/multiplayerServer.js
+++ public/scripts/gamePlay/multiplayerServer.js
@@ -1,1 +1,12 @@
 'use strict'
+const express = require('express')
+const app = express()
+const wSocket = require('ws')
+
+//set a headless wsServer that prints any new Event
+const wsServer = new ws.Server()
+wsServer.on('connection', socket => {
+    socket.on('message', message => console.log(message))
+})
+
+
