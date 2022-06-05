bc2043a8ce7380d059b8258c4ac17325dc6318b4
Danielbyte <danielmasango7@gmail.com>
2022-06-05 17:09:21 +0200

    server listen to port 8080

--- public/scripts/gamePlay/multiplayerServer.js
+++ public/scripts/gamePlay/multiplayerServer.js
@@ -1,1 +1,19 @@
 'use strict'
+const express = require('express')
+const fs = require('fs')
+const app = express()
+const http = require('http')
+const cors = require('cors')
+
+const PORT = process.env.PORT || 8080
+app.use(cors())
+
+const server = app.listen(PORT, () => {
+  console.log('Server is up')
+})
+
+const io = require('socket.io')(server, {
+  cors: {
+    origin: 'https://g15competitivewordle.azurewebsites.net/'
+  }
+})
