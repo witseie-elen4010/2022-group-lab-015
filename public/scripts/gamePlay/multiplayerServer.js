658722d8c600880eaa716b7a7260815052d07db5
Samkelo <samujola@gmail.com>
2022-06-05 16:51:07 +0200

    websocket server progress

--- public/scripts/gamePlay/multiplayerServer.js
+++ public/scripts/gamePlay/multiplayerServer.js
@@ -1,12 +1,23 @@
 'use strict'
+const { request } = require('express')
 const express = require('express')
 const app = express()
 const wSocket = require('ws')
 
-//set a headless wsServer that prints any new Event
-const wsServer = new ws.Server()
+//set a headless wsServer that prints any new incoming Event
+const wsServer = new wSocket.Server({noServer: true})
 wsServer.on('connection', socket => {
     socket.on('message', message => console.log(message))
+})
+
+//accepting a new client
+wsServer.on("request", request => {
+    const connection = request.accept(null, request.origin)
+    connection.on("open", () => console.log("open"))
+    connection.on("close", () => console.log("close"))
+    connection.on("message", message => {
+
+    })
 })
 
 
