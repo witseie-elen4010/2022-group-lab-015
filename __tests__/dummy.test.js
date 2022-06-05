8e6e443ee9bb46940978a3e86cf24f9a08473914
Samkelo <samujola@gmail.com>
2022-06-05 23:23:31 +0200

    server progress

--- __tests__/dummy.test.js
+++ __tests__/dummy.test.js
@@ -10,10 +10,10 @@ test('Greet: hello', () => {
 
 //server dummy test using a temp client! review this test
 //before submitting it
-test('', () => {
-  const ws = require('ws')
-  const client = new ws('ws://localhost:3000')
-  client.on('open', () => {
-    client.send('Hello')
-  })
-})
+// test('', () => { //pu name of test in brackets
+//   const ws = require('ws')
+//   const client = new ws('ws://localhost:3000')
+//   client.on('open', () => {
+//     client.send('Hello')
+//   })
+// })
