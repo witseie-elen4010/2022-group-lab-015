66d188dc194a4b5827f4befa51897acbd40e2369
Danielbyte <danielmasango7@gmail.com>
2022-06-06 07:11:08 +0200

    sucessfully stores room ID

--- public/scripts/gamePlay/dashBoard.js
+++ public/scripts/gamePlay/dashBoard.js
@@ -3,6 +3,12 @@
 const joinRoomButton = document.getElementById('join-room')
 
 joinRoomButton.addEventListener('click', () => {
-  const RoomNumber = document.getElementById('room-code')
-  console.log(RoomNumber)
+  const RoomNumber = document.getElementById('room-code').value
+  localStorage.setItem('roomcode', '')
+  if (RoomNumber === '') {
+    alert('Room code is required')
+  } else {
+    localStorage.setItem('roomcode', RoomNumber)
+    console.log(localStorage.getItem('roomcode'))
+  }
 })
